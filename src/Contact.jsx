001ee7fb640dd7555c9f1f37347cfcf954f3b9eb
Ishwarimahaddev <ishwarimahaddev@gmail.com>
2026-08-19 12:13:25 +0530

    first commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
+import './App.css'
 
 const Contact = () => {
   return (
-    <div style={{textAlign:'center'}}>
-      <div >
-        <h1>Contact</h1>
-      </div><br></br>
+    <div className="contact">
 
+      <h1>Contact</h1>
 
-        <div style={{display:'flex' ,gap:'30px',marginLeft:'180px'}}>
-      <div className='ID' >
-        <b >⭐Email⭐</b>
-        <div>Ishwarimahaddev@gmail.com</div>
-      </div>
+      <div className="contact-box">
 
-        <div className='ID' >
-            <b>⭐linkdln⭐</b>
-            <div>Ishwari Mahaddev</div>
-        </div>      
-        </div><br></br><br></br>
-
-        <div className='ID' style={{marginLeft:'300px'}}>
-            <b>⭐Github⭐</b>
-            <div>Ishwaeimahaddev</div>
+        <div className="ID">
+          <b>⭐ Email ⭐</b>
+          <div>Ishwarimahaddev@gmail.com</div>
         </div>
+
+        <div className="ID">
+          <b>⭐ LinkedIn ⭐</b>
+          <div>Ishwari Mahaddev</div>
+        </div>
+
+        <div className="ID">
+          <b>⭐ Github ⭐</b>
+          <div>Ishwaemahaddev</div>
+        </div>
+
+      </div>
 
     </div>
   )
